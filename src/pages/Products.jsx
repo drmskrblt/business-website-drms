f545b1d57b4e1e5a755594107825c75d1cd40025
DurmusK <durmusk@gmx.de>
2024-07-01 18:13:19 +0200

    Add products and services

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,119 @@
+import { consultingPricing, webdesignPricing } from "../data/planData";
+import { FaCheck } from "react-icons/fa6";
+
 const Products = () => {
   return (
-    <div>
-      Produkte
-    </div>
-  )
-}
+    <>
+      <section className="bg-primary-bg w-full p-6 text-white">
+        {/* Ueberschrift Produkte */}
+        <div className="text-center text-white p-4">
+          <h1 className="font-bold text-3xl">Webdesign & Erstellung</h1>
+          <p className=" mt-3 mx-auto">
+            Ein Plan für Webdesign & Erstellung auswählen
+          </p>
+        </div>
+        <div className="max-w-4xl mx-auto px-4">
+          <div className="grid gap-8 grid-cols-1 mt-10 md:grid-cols-3">
+            {webdesignPricing.map((item, index) => {
+              return (
+                <div key={index}>
+                  {/* Card Container */}
+                  <div className="p-8 border-2 border-gray-200 rounded-xl bg-secondary-bg shadow-lg flex flex-col">
+                    <h3 className="text-white text-lg font-semibold mb-1 text-center">
+                      {/* Card Heading */}
+                      {item.heading}
+                    </h3>
+                    <p className="flex items-baseline justify-center text-white mb-4">
+                      <span className="text-3xl font-extrabold">
+                        {item.preis}
+                      </span>
+                      {/* <span className="text-sm font-semibold ml-1">
+                    /monatlich
+                  </span> */}
+                    </p>
+                    <button className="btn text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                      Bestellen
+                    </button>
 
-export default Products
+                    {/* Plan Beschreibung */}
+                    <ul className="mt-8">
+                      {Object.values(item.beschreibung).map(
+                        (beschreibung, index) => (
+                          <li
+                            key={index}
+                            className="mb-3 text-gray-400 flex items-center text-sm"
+                          >
+                            <div className="">
+                              <FaCheck className="text-green-500 mr-3" />
+                            </div>
+                            <p>{beschreibung}</p>
+                          </li>
+                        )
+                      )}
+                    </ul>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+
+      {/* IT-Beratung */}
+
+      <section className="bg-primary-bg h-full p-4">
+        {/* Ueberschrift Produkte */}
+        <div className="text-center text-white p-4">
+          <h1 className="font-bold text-3xl">IT Beratung</h1>
+          <p className=" mt-3 mx-auto">Ein Plan für IT-Beratungen auswählen</p>
+        </div>
+        <div className="mx-auto px-4">
+          <div className="grid gap-8 grid-cols-1 mt-10 md:grid-cols-3">
+            {consultingPricing.map((item, index) => {
+              return (
+                <div key={index}>
+                  {/* Card Container */}
+                  <div className="p-8 border-2 border-gray-200 rounded-xl bg-secondary-bg shadow-lg flex flex-col">
+                    <h3 className="text-white text-lg font-semibold mb-1 text-center">
+                      {item.heading}
+                    </h3>
+                    <p className="flex items-baseline justify-center text-white mb-4">
+                      <span className="text-3xl font-extrabold">
+                        {item.preis}
+                      </span>
+                      {/* <span className="text-sm font-semibold ml-1">
+                    /monatlich
+                  </span> */}
+                    </p>
+                    <button className="btn text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                      Bestellen
+                    </button>
+
+                    {/* Plan Beschreibung */}
+                    <ul className="mt-8">
+                      {Object.values(item.beschreibung).map(
+                        (beschreibung, index) => (
+                          <li
+                            key={index}
+                            className="mb-3 text-gray-400 flex items-center text-sm"
+                          >
+                            <div className="mr-2">
+                              <FaCheck className="text-green-500 mr-3" />
+                            </div>
+                            <p>{beschreibung}</p>
+                          </li>
+                        )
+                      )}
+                    </ul>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Products;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,8 +1,42 @@
+import { serviceCards } from "../data/serviceData";
+
+
 const Services = () => {
+  // Styling of the section
+  const STYLE = {
+    sectionContainer: `bg-primary-bg w-full p-6 text-white`,
+  };
+
+  const CARD = {
+    container: `grid gap-6 py-4 md:grid-cols-2 lg:grid-cols-3`,
+    box: `bg-secondary-bg flex flex-col items-center p-3 rounded-lg shadow-md text-center text-white tracking-wide`,
+    icon: `text-[50px]`,
+    title: `text-2xl text-white font-bold m-2`,
+    button: `btn block mt-4 py-2 px-4 text-blue-200 font-bold border-2 border-blue-200 bg-transparent rounded-md text-center tracking-normal`,
+  };
+
   return (
-    <div>
-      Dienstleistungen
-    </div>
+    <>
+      {/* CARD SECTION */}
+      <section className={STYLE.sectionContainer}>
+        <div className={CARD.container}>
+          {serviceCards.map((card, index) => {
+            return (
+              <div key={index} >
+                <div className={CARD.box}>
+                  <div className={CARD.icon}>{card.icon}</div>
+                  <h3 className={CARD.title}>{card.title}</h3>
+                  <p>{card.description}</p>
+                  <a href={CARD.link} className={CARD.button}>
+                    {card.buttonText}
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </>
   )
 }
 
